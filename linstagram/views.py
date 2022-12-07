2a3fafae71b80dcec5f2234d06108dae3db6d12d
kristin <kristin@kristins-MacBook-Pro.local>
2022-12-07 17:48:38 +0900

    first linstagram

---
+++ linstagram/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+
+
+class Sub(APIView):
+    def get(self, request):
+        print("call get")
+        return render(request, "linstagram/main.html")
+
+    def post(self, request):
+        print("call post")
+        return render(request, "linstagram/main.html")
